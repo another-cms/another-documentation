8943a606306276b67df738d638f78f69c46a26c4
another-cms <chuckdurst67@gmail.com>
2018-03-08 23:27:37 +0100

    Updates book.json
    Auto commit by GitBook Editor

--- plugin.js
+++ plugin.js
@@ -1,9 +1,16 @@
 module.exports = {
+    // Map of hooks
+    hooks: {},
+
+    // Map of new blocks
     blocks: {
-        tag1: {
+    tag1: {
             process: function(block) {
                 return "Hello "+block.body+", How are you?";
             }
         }
-    }
+    },
+
+    // Map of new filters
+    filters: {}
 };
