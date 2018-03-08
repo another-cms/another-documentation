331d2fc6787a97bfc46b7004aafb458eb51c833f
another-cms <chuckdurst67@gmail.com>
2018-03-08 23:22:17 +0100

    Updates index.js
    Auto commit by GitBook Editor

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    blocks: {
+        tag1: {
+            process: function(block) {
+                return "Hello "+block.body+", How are you?";
+            }
+        }
+    }
+};
